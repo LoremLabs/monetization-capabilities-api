6112f66a8dc653894212d6a7040697196a0101b7
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-28 17:29:59 +0530

    feat(UserPreferences): add `matches` util fn

--- src/lib/UserPreferences.ts
+++ src/lib/UserPreferences.ts
@@ -14,6 +14,40 @@ export default class UserPreferences {
 		capabilities.forEach(c => this.#deny.add(c));
 	}
 
+	/**
+	 * @example assert(matches("foo/*", "foo/*"))
+	 * @example assert(matches("foo/bar", "foo/bar"))
+	 * @example assert(matches("foo/*", "foo/bar"))
+	 * @example assert(matches("foo/bar", "foo/*"))
+	 * @example assert(!matches("foo/*", "bar/*"))
+	 * @example assert(!matches("foo/bar", "foo/baz"))
+	 * @example assert(!matches("bar/bar", "baz/*"))
+	 */
+	matches(a: Capability, b: Capability): boolean {
+		return b.endsWith("/*") && !a.endsWith("/*")
+			? this.#matches(b, a)
+			: this.#matches(a, b);
+	}
+
+	#matches(a: Capability, b: Capability): boolean {
+		if (a === b) {
+			return true;
+		}
+		if (a.endsWith("/*") && b.endsWith("/*")) {
+			return a === b;
+		}
+
+		const [forPart, specificPart] = a.split("/", 2);
+		if (b.startsWith(`${forPart}/`) && specificPart === "*") {
+			return true;
+		}
+		return false;
+	}
+
+	denies(capability: Capability) {
+		return [...this.#deny].some(cap => this.matches(cap, capability));
+	}
+
 	get() {
 		return { prefers: [...this.#prefer], denies: [...this.#deny] };
 	}
