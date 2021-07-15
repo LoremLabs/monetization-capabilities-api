7a76bdfecbd0700ac7bf28331b47f6e1d8872693
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-07-15 18:02:34 +0530

    feat(UserPreferences): add update() method for bulk change

--- src/lib/UserPreferences.ts
+++ src/lib/UserPreferences.ts
@@ -9,27 +9,58 @@ class PreferenceChangeEvent extends Event {
 	}
 }
 
+class PreferenceUpdateEvent extends Event {
+	constructor(public changeType: "update", public detail?: any) {
+		super("change");
+	}
+}
+
 export default class UserPreferences extends EventTarget {
 	#allowList = new Set<Capability>();
 	#blockList = new Set<Capability>();
 
 	allow(capability: Capability) {
-		ensureValidCapability(capability);
-		this.#allowList.add(capability);
-		this.#blockList.delete(capability);
+		this.#allow(capability);
 		this.dispatchEvent(new PreferenceChangeEvent("allow", capability));
 	}
 
+	#allow(capability: Capability) {
+		ensureValidCapability(capability);
+		this.#allowList.add(capability);
+		this.#blockList.delete(capability);
+	}
+
 	deny(capability: Capability) {
+		this.#deny(capability);
+		this.dispatchEvent(new PreferenceChangeEvent("deny", capability));
+	}
+
+	#deny(capability: Capability) {
 		ensureValidCapability(capability);
 		this.#blockList.add(capability);
 		this.#allowList.delete(capability);
-		this.dispatchEvent(new PreferenceChangeEvent("deny", capability));
 	}
 
 	clear() {
 		this.#allowList.clear();
 		this.#blockList.clear();
+	}
+
+	update(
+		updateFn: (
+			currentValue: { [k in "allows" | "denies"]: Capability[] },
+		) => { [k in "allow" | "deny"]?: Capability[] },
+	) {
+		const { allow, deny } = updateFn(this.get()) || {};
+		if (Array.isArray(allow)) {
+			this.#allowList.clear();
+			allow.forEach(cap => this.#allow(cap));
+		}
+		if (Array.isArray(deny)) {
+			this.#blockList.clear();
+			deny.forEach(cap => this.#deny(cap));
+		}
+		this.dispatchEvent(new PreferenceUpdateEvent("update"));
 	}
 
 	/**
