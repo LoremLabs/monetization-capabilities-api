c8a8cd2b3760e1aa20723ad4d5dc2c42e2706bf5
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-07-20 16:31:16 +0530

    BREAKING CHANGE: rename monetization to monet
    to avoid potential clashes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import Monetization from "./lib/Monetization.js";
 
-const monetization = new Monetization();
-Object.defineProperty(window, "monetization", {
+Object.defineProperty(window, "monet", {
 	writable: false,
 	configurable: false,
-	value: monetization,
+	value: new Monetization(),
 });
